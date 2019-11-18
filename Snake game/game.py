055a3cb06310dfe07651706e34eeac063d48942f
lucasmartin96 <lucasmartin766@gmail.com>
2019-11-18 01:22:53 -0300

    Arreglo de bugs

--- Snake game/game.py
+++ Snake game/game.py
@@ -1,3 +1,5 @@
+# Snake Tutorial Python
+
 import math
 import random
 import pygame
@@ -7,31 +9,37 @@ from tkinter import messagebox
 
 class cubo(object):
     filas = 20
-    ancho = 500
+    a = 500
 
-    def __init__(self, inicio, dirx=1, diry=0, color=(255, 0, 0)):
-        self.pos = inicio
-        self.dirx = dirx
-        self.diry = diry
+    def __init__(self, start, dirx=1, diry=0, color=(255, 0, 0)):
+        self.pos = start
+        self.dirx = 1
+        self.diry = 0
         self.color = color
 
     def mover(self, dirx, diry):
         self.dirx = dirx
         self.diry = diry
-        self.pos(self.pos[0] + self.dirx, self.pos[1] + self.diry)
+        self.pos = (self.pos[0] + self.dirx, self.pos[1] + self.diry)
 
     def dibujar(self, sup, ojos=False):
-        dis = self.ancho // self.filas
+        dis = self.a // self.filas
         i = self.pos[0]
         j = self.pos[1]
-        pygame.draw.rect(sup, self.color, (i*dis+1, j*dis+1, dis-2, dis-2))
 
-
+        pygame.draw.rect(sup, self.color, (i * dis + 1, j * dis + 1, dis - 2, dis - 2))
+        if ojos:
+            centre = dis // 2
+            radio = 3
+            ojo1 = (i * dis + centre - radio, j * dis + 8)
+            ojo2 = (i * dis + dis - radio * 2, j * dis + 8)
+            pygame.draw.circle(sup, (0, 0, 0), ojo1, radio)
+            pygame.draw.circle(sup, (0, 0, 0), ojo2, radio)
 
 
 class snake(object):
     cuerpo = []
-    mov = []
+    movs = {}
 
     def __init__(self, color, pos):
         self.color = color
@@ -44,118 +52,155 @@ class snake(object):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
+
             keys = pygame.key.get_pressed()
-            # Controles
+
             for key in keys:
-                if key[pygame.K_LEFT]:
+                if keys[pygame.K_LEFT]:
                     self.dirx = -1
                     self.diry = 0
-                    self.mov[self.cabeza.pos[:]] = [self.dirx, self.diry]
-                elif key[pygame.K_RIGHT]:
+                    self.movs[self.cabeza.pos[:]] = [self.dirx, self.diry]
+
+                elif keys[pygame.K_RIGHT]:
                     self.dirx = 1
                     self.diry = 0
-                    self.mov[self.cabeza.pos[:]] = [self.dirx, self.diry]
-                elif key[pygame.K_UP]:
+                    self.movs[self.cabeza.pos[:]] = [self.dirx, self.diry]
+
+                elif keys[pygame.K_UP]:
                     self.dirx = 0
                     self.diry = -1
-                    self.mov[self.cabeza.pos[:]] = [self.dirx, self.diry]
-                elif key[pygame.K_DOWN]:
+                    self.movs[self.cabeza.pos[:]] = [self.dirx, self.diry]
+
+                elif keys[pygame.K_DOWN]:
                     self.dirx = 0
-                    self.diry = -1
-                    self.mov[self.cabeza.pos[:]] = [self.dirx, self.diry]
+                    self.diry = 1
+                    self.movs[self.cabeza.pos[:]] = [self.dirx, self.diry]
 
         for i, c in enumerate(self.cuerpo):
             p = c.pos[:]
-            if p in self.mov:
-                mov = self.mov[p]
-                c.mover(mov[0], mov[i])
-                # Elimina el ultimo cubo
+            if p in self.movs:
+                mov = self.movs[p]
+                c.mover(mov[0], mov[1])
                 if i == len(self.cuerpo) - 1:
-                    self.mov.pop(p)
-
-                # Chequear el final de la pantalla
+                    self.movs.pop(p)
+            else:
+                if c.dirx == -1 and c.pos[0] <= 0:
+                    c.pos = (c.filas - 1, c.pos[1])
+                elif c.dirx == 1 and c.pos[0] >= c.filas - 1:
+                    c.pos = (0, c.pos[1])
+                elif c.diry == 1 and c.pos[1] >= c.filas - 1:
+                    c.pos = (c.pos[0], 0)
+                elif c.diry == -1 and c.pos[1] <= 0:
+                    c.pos = (c.pos[0], c.filas - 1)
                 else:
-
-                    # Si se mueve hacia la izquierda y la posicion x es menor o igual a 0(borde izquierdo de la
-                    # pantalla), se cambia la posicion hacia el borde derecho de la pantalla
-
-                    if c.dirx == -1 and c.pos[0] <= 0:
-                        c.pos = (c.filas - 1, c.pos[1])
-
-                    # Si se mueve hacia la derecha y la posicion x es mayor o igual al borde de la pantalla, se cambia
-                    # la posicion hacia el borde izquierdo
-
-                    elif c.dirx == 1 and c.pos[0] >= c.filas - 1:
-                        c.pos = (0, c.pos[1])
-                    # Si se mueve hacia abajo y la posicion y es mayor o igual al borde inferior de la pantalla, se
-                    # cambia la posicion hacia el superior
-                    elif c.diry == 1 and c.pos[1] >= c.filas - 1:
-                        c.pos = (c.pos[0], 0)
-
-                    # Si se mueve hacia arriba y la posicion es menor o igual al borde superior de la pantalla,
-                    # se cambia la posicion hacia el inferior.
-                    elif c.diry == -1 and c.pos[1] <= 0:
-                        c.pos = (c.pos[0], c.filas - 1)
-
-                    # Si no sigue moviendose a la direccion que deberia
-                    else:
-                        c.mover(c.dirx, c.diry)
+                    c.mover(c.dirx, c.diry)
 
     def reset(self, pos):
-        pass
+        self.cabeza = cubo(pos)
+        self.cuerpo = []
+        self.cuerpo.append(self.cabeza)
+        self.movs = {}
+        self.dirx = 0
+        self.diry = 1
 
     def addCubo(self):
-        pass
+        cola = self.cuerpo[-1]
+        dx, dy = cola.dirx, cola.diry
 
-    def dibujar(self, sup):
+        if dx == 1 and dy == 0:
+            self.cuerpo.append(cubo((cola.pos[0] - 1, cola.pos[1])))
+        elif dx == -1 and dy == 0:
+            self.cuerpo.append(cubo((cola.pos[0] + 1, cola.pos[1])))
+        elif dx == 0 and dy == 1:
+            self.cuerpo.append(cubo((cola.pos[0], cola.pos[1] - 1)))
+        elif dx == 0 and dy == -1:
+            self.cuerpo.append(cubo((cola.pos[0], cola.pos[1] + 1)))
+
+        self.cuerpo[-1].dirx = dx
+        self.cuerpo[-1].diry = dy
+
+    def dibujar(self, surface):
         for i, c in enumerate(self.cuerpo):
             if i == 0:
-                c.dibujar(sup, True)
+                c.dibujar(surface, True)
             else:
-                c.dibujar(sup)
+                c.dibujar(surface)
 
 
-def dibujarGrid(ancho, filas, sup):
-    tamanio = ancho // filas
+def dibujarGrid(w, rows, surface):
+    tamanio = w // rows
+
     x = 0
     y = 0
-    for i in range(filas):
-        x += tamanio
-        y += tamanio
-        pygame.draw.line(sup, (255, 255, 255), (x, 0), (x, ancho))
-        pygame.draw.line(sup, (255, 255, 255), (0, y), (ancho, y))
+    for l in range(rows):
+        x = x + tamanio
+        y = y + tamanio
+
+        pygame.draw.line(surface, (255, 255, 255), (x, 0), (x, w))
+        pygame.draw.line(surface, (255, 255, 255), (0, y), (w, y))
 
 
 def reDibujarVentana(sup):
-    global filas, ancho, s
+    global filas, ancho, s, snack
     sup.fill((0, 0, 0))
     s.dibujar(sup)
+    snack.dibujar(sup)
     dibujarGrid(ancho, filas, sup)
     pygame.display.update()
 
 
-def randomComida(filas, items):
-    pass
+def randomComida(f, item):
+    posiciones = item.cuerpo
+
+    while True:
+        x = random.randrange(f)
+        y = random.randrange(f)
+        if len(list(filter(lambda z: z.pos == (x, y), posiciones))) > 0:
+            continue
+        else:
+            break
+
+    return (x, y)
 
 
 def message_box(asunto, contenido):
-    pass
+    root = tk.Tk()
+    root.attributes("-topmost", True)
+    root.withdraw()
+    messagebox.showinfo(asunto, contenido)
+    try:
+        root.destroy()
+    except:
+        pass
 
 
 def main():
-    global ancho, filas, s
+    global ancho, filas, s, snack
     ancho = 500
     filas = 20
-    ventana = pygame.display.set_mode((ancho, ancho))
+    win = pygame.display.set_mode((ancho, ancho))
     s = snake((255, 0, 0), (10, 10))
+    snack = cubo(randomComida(filas, s), color=(0, 255, 0))
     flag = True
-    reloj = pygame.time.Clock()
-    while flag:
-        # Control de velocidad de juego
-        pygame.time.delay(50)
-        reloj.tick(10)
 
-        reDibujarVentana(ventana)
+    clock = pygame.time.Clock()
+
+    while flag:
+        pygame.time.delay(50)
+        clock.tick(10)
+        s.mover()
+        if s.cuerpo[0].pos == snack.pos:
+            s.addCubo()
+            snack = cubo(randomComida(filas, s), color=(0, 255, 0))
+
+        for x in range(len(s.cuerpo)):
+            if s.cuerpo[x].pos in list(map(lambda z: z.pos, s.cuerpo[x + 1:])):
+                print('Score: ', len(s.cuerpo))
+                message_box('You Lost!', 'Play again...')
+                s.reset((10, 10))
+                break
+
+        reDibujarVentana(win)
 
 
 main()
